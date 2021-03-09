f96e0e709d0f11ab96b07b9328c046b1af7db57a
RichardHuszar <huszirics10@gmail.com>
2021-03-09 10:20:35 +0100

    Törlés javítva, Módosítás kész

--- telefonkonyv/MySqlDB.php
+++ telefonkonyv/MySqlDB.php
@@ -9,10 +9,14 @@ class MySqlDB {
     private $kapcs;
 
     public function __construct() {
-        $this->db_szerver = "localhost";
-        $this->db_felhnev = "root";
-        $this->db_jelszo = "";
-        $this->db_nev = "telefonkonyv2";
+//        $this->db_szerver = "localhost";
+//        $this->db_felhnev = "root";
+//        $this->db_jelszo = "";
+//        $this->db_nev = "telefonkonyv2";
+        $this->db_szerver = "tanulo15.szf1a.oktatas.szamalk-szalezi.hu";
+        $this->db_felhnev = "c1_tanulo15szf1a";
+        $this->db_jelszo = "_tanulo15szf1a";
+        $this->db_nev = "c1ABtanulo15szf1a";
         $this->kapcsolat();
     }
 
--- telefonkonyv/js.js
+++ telefonkonyv/js.js
@@ -1,27 +1,29 @@
 $(function () {
 
-    $("#beolvas").on("click", beolvas);
+    $("#beolvas").on("click", adatBeolvas);
     $("#kuld").on("click", adBeir);
+    $("article").delegate(".torol","click",adTorol);
+    $("article").delegate(".szerkeszt","click",adatSzerkeszt);
+    $("megse").on("click",adatMegse);
+    $("#modosit").on("click",adatModosit);
 
 });
-
+var telefonkonyvem = [];
 function kiir() {
-//    var nev = $("nev").val();
-//    var tel = $("tel").val();
-//    var kep = $("kep").val();
     $("article").empty();
     for (var i = 0; i < telefonkonyvem.length; i++) {
-        var nev= telefonkonyvem[i].nev;
+        var ID= telefonkonyvem[i].ID;
+         var nev= telefonkonyvem[i].nev;
         var tel=telefonkonyvem[i].tel;
         var kep=telefonkonyvem[i].kep;
-          var elem = "<div> <h2>" + nev + "</h2> <p>" + tel + "</p> <p>" + kep + " </p><button class='torol'>Töröl </button> </div> ";
+          var elem = "<div> <h2>" + nev + "</h2> <p>" + tel + "</p> <p>" + kep + " </p><button class='torol'id='"+ID+"'>Töröl </button> </div> <button id ='" + i +"' class='szerkeszt'>Szerkeszt</button> <hr> </div>"; ;
     $("article").append(elem);
     
     }
   
 }
 
-function beolvas(){
+function adatBeolvas(){
     $.ajax({
         type: "GET",
         url: "feldolgoz.php",
@@ -59,4 +61,58 @@ function adBeir(){
         }
     });
 }
+function adTorol(){
+    var aktelem = $(this).closest("div");
+    var id= $(this).attr("id");
+    console.log("Törlés***********"+id);
+    $.ajax({
+        type: "DELETE",
+        url: "torles.php?ID="+id,
+        success: function (){
+            console.log("törlés");
+            aktelem.remove();
+        },
+error:function (){
+    alert("Hiba adatok törlésekor!");
+}
+    });
+    }
 
+function adatSzerkeszt(){
+    $(".szerkesztes").removeClass("elrejt");
+    
+    var index = $(this).attr("id");
+    console.log(index);
+    $("#id2").val(telefonkonyvem[index].ID);
+    $("#nev2").val(telefonkonyvem[index].nev);
+    $("#tel2").val(telefonkonyvem[index].tel);
+    $("#kep2").val(telefonkonyvem[index].kep);
+}
+
+function adatMegse(){
+    $(".szerkesztes").addClass("elrejt");
+   
+}
+
+function adatModosit(){
+    var editSzemely = {
+        ID: $("#id2").val(),
+        nev: $("#nev2").val(),
+        tel:  $("#tel2").val(),
+        kep: $("#kep2").val()
+    };
+    
+    $.ajax({
+        type: "PUT",
+        url: "modosit.php",
+        data: editSzemely,
+        success: function (){
+            adatBeolvas();
+        },
+        error: function () {
+            alert("Hiba az adatok modosításakor!");
+        }
+    });
+
+    
+}
